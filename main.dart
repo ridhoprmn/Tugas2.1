d4ee652ea2ee1aa6674a74b741c604ccd5c11742
ridhoprmn <80235752+ridhoprmn@users.noreply.github.com>
2021-03-28 02:18:10 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_2/cmn.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Good());
+  }
+}
